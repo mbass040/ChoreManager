ecc62a05fdc270f16a0c607d948f86be12d15969
MilenaDionne <32244738+MilenaDionne@users.noreply.github.com>
2017-11-22 18:01:20 -0500

    create all equipment

--- TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/MainActivity.java
+++ TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onBackPressed() { //when the user clicks the back button on his Android
+    public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
@@ -107,27 +107,24 @@ public class MainActivity extends AppCompatActivity
 //        } else if (id == R.id.nav_manage) {
 //
 //        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
+        if (id == R.id.nav_broom) {
+            Intent intent = new Intent(getApplicationContext(),EquipmentActivity.class);
+            startActivityForResult(intent,0);
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
-    // activated when a user clicks on teh Switch User button in the navigation drawer
     public void btnChangeUser(View view){
         txtName = (TextView) findViewById(R.id.txtUser);
 
-        //hardcoded for now but later to be added dynamically
         final String[] items = new String[] {"Rasheed Wallace"
                 , "Ben Wallace"
                 , "Christopher Wallace"
                 , "DeAndre Wallace"
                 , "Bonifa Jackson"
-                , "***NEW USER***"
         };
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -135,11 +132,7 @@ public class MainActivity extends AppCompatActivity
         builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                if(items[which].equals("***NEW USER***")){ // opens a new window to with a form for the new user
-                    Intent newUserIntent = new Intent(MainActivity.this, NewUserActivity.class);
-                    startActivity(newUserIntent);
-                }else
-                    txtName.setText(items[which]);
+                txtName.setText(items[which]);
             }
         });
         AlertDialog alert = builder.create();
@@ -154,4 +147,5 @@ public class MainActivity extends AppCompatActivity
 
         startActivityForResult(intent, RESULT_OK);
     }
+
 }
--- TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/TaskManagerDatabaseHandler.java
+++ TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/TaskManagerDatabaseHandler.java
@@ -17,29 +17,12 @@ public class TaskManagerDatabaseHandler extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "taskManagerDB.db";
     // Table name
     public static final String TABLE_USERS = "users";
-    public static final String TABLE_TASK = "task";
-    public static final String TABLE_ITEM = "item";
-    public static final String TABLE_CART_ITEM = "cart_item";
+
 
     // User Table Column Names
     public static final String USER_ID = "user_id";
     public static final String USER_NAME = "user_name";
 
-    // Task Table Column Names
-    public static final String TASK_ID = "task_id";
-    public static final String TASK_NAME = "task_name";
-    public static final String TASK_NOTES = "task_notes";
-    public static final String TASK_DEADLINE = "task_deadline";
-    public static final String TASK_POINT_VALUE = "task_point_value";
-    public static final String TASK_STATUS = "task_status";
-
-    // Item Table Column Names
-    public static final String ITEM_ID = "item_id";
-    public static final String ITEM_NAME = "item_name";
-
-    // Cart Item Column Names
-    public static final String CART_ITEM_ID = "cart_item_id";
-    public static final String CART_ITEM_TYPE = "cart_item_type";
 
     public TaskManagerDatabaseHandler(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
