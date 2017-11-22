366a4c2fa22a01374c9a96e92f13e0800f16949a
Max <mbass040@uottawa.ca>
2017-11-21 22:19:55 -0500

    Added more details to the Specific Task view

--- TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/SpecificTaskActivity.java
+++ TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/SpecificTaskActivity.java
@@ -1,9 +1,11 @@
 package seg2105.uottawa.com.taskmanager;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,12 +24,26 @@ public class SpecificTaskActivity extends AppCompatActivity {
         Task task = new Task("Clean Pool");
 
         setTitle(task.getName());
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabEditTask);
     }
 
     public void editTask(View view) {
 
+    }
+
+    public void showMoreTaskOptions(View view) {
+
+        // Build and show the dialog containing options for a specific task (finish, delete, unassign, etc.)
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.taskOptionTitle);
+        builder.setItems(R.array.taskOptions, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 }
