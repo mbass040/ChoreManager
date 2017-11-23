8b4dcb4c4b89bd39ee6402e07f0ea909a0a37077
Gabriel Karkaji <gabriel.karkaji@gmail.com>
2017-11-23 15:08:46 -0500

    Revert "Second try?..."
    This reverts commit 3c3909de4583267a3406b13f22bc5ef81febcd31, reversing
    changes made to 0aea759bd0a78569147936e791bd328431745f26.

--- TaskManager/app/src/androidTest/java/seg2105/uottawa/com/taskmanager/ExampleInstrumentedTest.java
+++ TaskManager/app/src/androidTest/java/seg2105/uottawa/com/taskmanager/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("seg2105.uottawa.com.taskmanager", appContext.getPackageName());
+        assertEquals("seg2105.uottawa.com.choremanager", appContext.getPackageName());
     }
 }
--- TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/source/User.java
+++
@@ -1,8 +0,0 @@
-package seg2105.uottawa.com.taskmanager.source;
-
-/**
- * Created by Max on 2017-11-20.
- */
-
-public class User {
-}
