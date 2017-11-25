c3a82872f8f4afb460bb7eb849cc965bbea1ebd3
Gabriel Karkaji <gabriel.karkaji@gmail.com>
2017-11-25 13:14:29 -0500

    Modified TaskList.java

--- TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/TaskList.java
+++ TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/TaskList.java
@@ -1,50 +1,52 @@
 package seg2105.uottawa.com.taskmanager;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
+
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-
-import static seg2105.uottawa.com.taskmanager.R.id.lvTaskList;
+import android.widget.TextView;
 
 
-
-public class TaskList extends AppCompatActivity {
+public class TaskList extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
 
-        ListView taskListView = (ListView) findViewById(lvTaskList);
-        HashMap<String, String> taskName = new HashMap<>();
 
-        taskName.put("Shopping", "17 items in List");
-        taskName.put("Vaccum Living Room", "Deadline: Tonight - Unassigned");
-        taskName.put("Wash Car", "Note: Don't wash if it rains tonight");
-        taskName.put("Wash Dishes", "Repeat: Daily");
-        taskName.put("Call Veterinary", "Note: Urgent");
+        final List<String[]> taskList = new LinkedList<String[]>();
+        taskList.add(new String []{"Shopping", "17 items in List"});
+        taskList.add(new String []{"Vaccum Living Room", "Deadline: Tonight - Unassigned"});
+        taskList.add(new String []{"Wash Car", "Note: Don't wash if it rains tonight"});
+        taskList.add(new String []{"Wash Dishes", "Repeat: Daily"});
+        taskList.add(new String []{"Call Veterinary", "Note: Urgent"});
 
-        List<HashMap<String, String>> listItems = new ArrayList<>();
-        SimpleAdapter adapter = new SimpleAdapter(this, listItems, R.layout.list_item, new String[]{"First Line", "Second Line"}, new int []{R.id.txt1, R.id.txt2});
+        ArrayAdapter<String[]> adapter = new ArrayAdapter<String[]>(this, android.R.layout.simple_list_item_2, android.R.id.text1, taskList){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                View view = super.getView(position, convertView, parent);
 
-        Iterator iter = taskName.entrySet().iterator();
-        while(iter.hasNext()){
-            HashMap<String, String> taskMap = new HashMap<>();
-            Map.Entry pair = (Map.Entry) iter.next();
-            taskMap.put("First line", pair.getKey().toString());
-            taskMap.put("Second line", pair.getValue().toString());
-            listItems.add(taskMap);
-        }
+                String entry[] = taskList.get(position);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+                text1.setText(entry[0]);
+                text2.setText(entry[1]);
 
-        taskListView.setAdapter(adapter);
+                return view;
+            }
 
+        };
+
+
+        setListAdapter(adapter);
     }
+
 
 }
