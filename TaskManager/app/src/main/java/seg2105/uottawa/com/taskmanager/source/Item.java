a3f823f900b58ff5a5b91272ff4febd3d726c783
Samuel Bédard <32080094+bedardsam@users.noreply.github.com>
2017-11-25 12:36:09 -0500

    change on item and creation of cartitem

--- TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/source/Item.java
+++ TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/source/Item.java
@@ -5,10 +5,13 @@ package seg2105.uottawa.com.taskmanager.source;
  */
 
 public class Item {
-    private String itemName = "Name Not Defined";
+    protected String itemName = "Name Not Defined";
 
-    public Item(String name, String description) {
+    public Item(String name) {
         this.itemName = name;
+    }
+    public Item(){
+        itemName = "";
     }
 
     //Here would go some code to manage the information in this class
