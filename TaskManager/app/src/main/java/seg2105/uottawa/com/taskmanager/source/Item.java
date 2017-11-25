c988214235b27930f1fb5e7527ffc774154b0086
MilenaDionne <32244738+MilenaDionne@users.noreply.github.com>
2017-11-25 12:14:51 -0500

    rename equipment

---
+++ TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/source/Item.java
@@ -0,0 +1,23 @@
+package seg2105.uottawa.com.taskmanager.source;
+
+/**
+ * Created by milena_dionnne on 2017-11-22.
+ */
+
+public class Item {
+    private String itemName = "Name Not Defined";
+
+    public Item(String name, String description) {
+        this.itemName = name;
+    }
+
+    //Here would go some code to manage the information in this class
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String newName) {
+         itemName = newName;
+    }
+
+}
