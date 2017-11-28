82d36d99ff54f961a5440c59f14d8bd7a6280285
Samuel Bédard <32080094+bedardsam@users.noreply.github.com>
2017-11-28 16:21:19 -0500

    btnadd for shoppingList

--- TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/source/ShoppingList.java
+++ TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/source/ShoppingList.java
@@ -1,12 +1,17 @@
 package seg2105.uottawa.com.taskmanager.source;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,37 +21,38 @@ import seg2105.uottawa.com.taskmanager.R;
 
 public class ShoppingList extends AppCompatActivity {
 
-    List<String> grocerieList;
-    List<String> materialList;
+    private List<String> groceryList;
+    private List<String> materialList;
+    private ArrayAdapter<String> groceryAdapter;
+    private ArrayAdapter<String> materialAdapter;
+    private int selection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.btnAddToCart);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //todo
+                addDialog(view);
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        grocerieList = new ArrayList<String>();
+        groceryList = new ArrayList<String>();
         materialList = new ArrayList<String>();
 
-        grocerieList.add("pototo");
-        grocerieList.add("allo");
-        grocerieList.add("padfad");
-        grocerieList.add("pototo");
-        grocerieList.add("allo");
-        grocerieList.add("padfad");
-        grocerieList.add("pototo");
-        grocerieList.add("allo");
-        grocerieList.add("padfad");
+        groceryList.add("pototo");
+        groceryList.add("allo");
+        groceryList.add("padfad");
+        groceryList.add("pototo");
+        groceryList.add("allo");
+        groceryList.add("padfad");
+        groceryList.add("pototo");
+        groceryList.add("allo");
+        groceryList.add("padfad");
 
 
         materialList.add("things");
@@ -59,13 +65,58 @@ public class ShoppingList extends AppCompatActivity {
         materialList.add("things");
 
         ListView lvGrocerie = (ListView) findViewById(R.id.lvGrocerie);
-        ArrayAdapter<String> grocerieAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, grocerieList);
-        lvGrocerie.setAdapter(grocerieAdapter);
+        groceryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, groceryList);
+        lvGrocerie.setAdapter(groceryAdapter);
 
 
         ListView lvMaterial = (ListView) findViewById(R.id.lvMaterial);
-        ArrayAdapter<String> materialAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, materialList);
+        materialAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, materialList);
         lvMaterial.setAdapter(materialAdapter);
+    }
+    public void addDialog(View view){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.shopdialog);
+
+        CharSequence[] choice = {"Grocery","Material"};
+
+        builder.setSingleChoiceItems(choice,0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selection = which;
+            }
+        });
+
+        builder.setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                add("allo");
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void modifyDialog(){
+
+    }
+
+    private void add(String item){
+        if(selection == 0){
+            materialList.add(item);
+            materialAdapter.notifyDataSetChanged();
+
+        }else{
+            groceryList.add(item);
+            groceryAdapter.notifyDataSetChanged();
+        }
     }
 
 }
