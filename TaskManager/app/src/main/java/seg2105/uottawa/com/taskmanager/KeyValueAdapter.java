39be6a563adcc9849e92e6fdd96ee5902748d814
Max <mbass040@uottawa.ca>
2017-11-28 23:54:12 -0500

    Updates to Task Options dialog; prepping to handle actions

---
+++ TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/KeyValueAdapter.java
@@ -0,0 +1,64 @@
+package seg2105.uottawa.com.taskmanager;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Max on 2017-11-28.
+ */
+
+public class KeyValueAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private List<String[]> mElems;
+
+    // Gets the context so it can be used later
+    public KeyValueAdapter(Context c, ArrayList<String[]> list) {
+        mContext = c;
+        mElems = list;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return Integer.valueOf(mElems.get(position)[0]);
+    }
+
+    @Override
+    public int getCount() {
+        return mElems.size();
+    }
+
+    public View getView(final int position, View convertView, final ViewGroup parent) {
+        View listView;
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+            listView = inflater.inflate(R.layout.key_value_listitem, null);
+            final TextView lblID = (TextView) listView.findViewById(R.id.lblID);
+            TextView lblValue = (TextView) listView.findViewById(R.id.lblValue);
+
+            lblID.setText(String.valueOf(mElems.get(position)[0]));
+            lblValue.setText(String.valueOf(mElems.get(position)[1]));
+        }
+        else {
+            listView = convertView;
+        }
+
+        return listView;
+    }
+
+}
