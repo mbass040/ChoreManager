add95071f9cac46fa091915f9d5a90284e3aabe0
Samuel Bédard <32080094+bedardsam@users.noreply.github.com>
2017-12-02 14:01:39 -0500

    shopping list comment

--- TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/source/ShoppingList.java
+++ TaskManager/app/src/main/java/seg2105/uottawa/com/taskmanager/source/ShoppingList.java
@@ -24,9 +24,11 @@ public class ShoppingList extends AppCompatActivity {
     //list of item
     private List<Item> groceryList;
     private List<Item> materialList;
+
     //adapter for item
     private ItemList groceryAdapter;
     private ItemList materialAdapter;
+
     //item type
     private enum itemType {Equipment, CartItem};
 
@@ -53,9 +55,10 @@ public class ShoppingList extends AppCompatActivity {
                 addDialog(view);
             }
         });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //initiate both list and database
+        //initiate the lists and database
         groceryList = new ArrayList<Item>();
         materialList = new ArrayList<Item>();
         db = new TaskManagerDatabaseHandler(this);
@@ -102,18 +105,28 @@ public class ShoppingList extends AppCompatActivity {
             }
         });
 
+        //show the listView
         update();
 
     }
+
     private void addDialog(View view){
+
+        //create a builder
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //set the title
         builder.setTitle(R.string.shoppingAdd);
+
+        //set the view of the dialog
         final View dialogview = getLayoutInflater().inflate(R.layout.nav_add_to_shoppin,null);
         builder.setView(dialogview);
+
+        //get id in the view
         final Spinner spinner = (Spinner) dialogview.findViewById(R.id.spinnerShopOption);
         final EditText text = (EditText) dialogview.findViewById(R.id.txtShoppingOption);
 
-
+        //set the add button
         builder.setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -123,6 +136,8 @@ public class ShoppingList extends AppCompatActivity {
                 dialog.dismiss();
             }
         });
+
+        //set the cancel button
         builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -130,20 +145,28 @@ public class ShoppingList extends AppCompatActivity {
             }
         });
 
-
+        //show the dialog
         AlertDialog dialog = builder.create();
         dialog.show();
     }
 
     private void modifyDialog(final int position, String name){
+
+        //create builder
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //set title
         builder.setTitle(R.string.shoppingModify);
+
+        //set view
         final View dialogview = getLayoutInflater().inflate(R.layout.nav_modify_shopping,null);
         builder.setView(dialogview);
+
+        //get id int the view
         final EditText text = (EditText) dialogview.findViewById(R.id.txtmodifyshopping);
         text.setText(name);
 
-
+        //set the modify button
         builder.setPositiveButton(R.string.modify, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -152,6 +175,8 @@ public class ShoppingList extends AppCompatActivity {
                 dialog.dismiss();
             }
         });
+
+        //set the cancel button
         builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -159,15 +184,47 @@ public class ShoppingList extends AppCompatActivity {
             }
         });
 
+        //show dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 
+    private void deleteDialog(final int position){
+
+        //create builder
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //set title
+        builder.setTitle(R.string.deleteItem);
+
+        //set delete button
+        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                delete(position);
+                update();
+                dialog.dismiss();
+            }
+        });
+
+        //set cancel button
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        //show dialog
         AlertDialog dialog = builder.create();
         dialog.show();
     }
 
     private void add(String item,int type){
-        if(type == 1){
+
+        if(type == 1){ // if it's a material it add to the list of material
             db.insertItem(TaskManagerDatabaseHandler.DBItemType.CartItem,item,CartItem.ItemType.Material);
-        }else{
+        }else{ // if it's a grocery it add it to the list of grocery
             db.insertItem(TaskManagerDatabaseHandler.DBItemType.CartItem,item,CartItem.ItemType.Grocery);
         }
     }
@@ -175,16 +232,21 @@ public class ShoppingList extends AppCompatActivity {
     private void update(){
         //get value from the database
         List<Item> list = db.getItems(false);
+
         CartItem cartItem;
         CartItem.ItemType type;
+
+        //clear the list of grocery and material
         materialList.clear();
         groceryList.clear();
-        while(!list.isEmpty()){
+
+        //put the value in the right list
+        while(!list.isEmpty()){//stop when the list is empty
             cartItem =(CartItem) list.remove(0);
             type = cartItem.getIsAMaterial();
-            if(type == CartItem.ItemType.Grocery){
+            if(type == CartItem.ItemType.Grocery){//if grocery add to grocery
                 groceryList.add(cartItem);
-            }else{
+            }else{  //if material add to material
                 materialList.add(cartItem);
             }
         }
@@ -195,42 +257,20 @@ public class ShoppingList extends AppCompatActivity {
         lvGrocery.setAdapter(groceryAdapter);
         lvMaterial.setAdapter(materialAdapter);
     }
+
     private void modify(int position,String name){
-        if(type == CartItem.ItemType.Grocery){
+
+        if(type == CartItem.ItemType.Grocery){//if grocery modify the item in grocery
             db.updateItem(groceryList.get(position).getID(),name);
-        }else{
+        }else{ //if material modify the item in material
             db.updateItem(materialList.get(position).getID(),name);
         }
     }
 
-    private void deleteDialog(final int position){
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.deleteItem);
-
-        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                delete(position);
-                update();
-                dialog.dismiss();
-            }
-        });
-        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
-
-        AlertDialog dialog = builder.create();
-        dialog.show();
-    }
-
     private void delete(int position){
-        if(type == CartItem.ItemType.Grocery){
+        if(type == CartItem.ItemType.Grocery){ // if grocery delete the item in grocery
             db.deleteItem(groceryList.get(position).getID());
-        }else{
+        }else{ // material delete the item in material
             db.deleteItem(materialList.get(position).getID());
         }
     }
